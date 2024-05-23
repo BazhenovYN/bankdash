f8190a2edde7fe94eac50726bc6e6cb03e641f4a
Iurii Bazhenov <bazhenovyn@gmail.com>
2024-05-23 13:05:40 +0200

    fix: add priority attribute for avatar image

--- src/components/common/avatar/avatar.tsx
+++ src/components/common/avatar/avatar.tsx
@@ -13,7 +13,7 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 export function Avatar({ src, width, height, alt = '', className, children }: Readonly<Props>) {
   return (
     <div className={clsx('overflow-hidden rounded-full', className)}>
-      {src ? <Image src={src} alt={alt} width={width} height={height} /> : <>{children}</>}
+      {src ? <Image src={src} alt={alt} width={width} height={height} priority /> : <>{children}</>}
     </div>
   );
 }

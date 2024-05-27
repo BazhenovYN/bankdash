8d675202508a09ebbc55811a06be45a2a95a262a
Iurii Bazhenov <bazhenovyn@gmail.com>
2024-05-28 01:13:58 +0200

    fix: fix incorrect handling searchParams

--- src/components/ui/tabs/tab.tsx
+++ src/components/ui/tabs/tab.tsx
@@ -1,26 +1,33 @@
 'use client';
 
 import clsx from 'clsx';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 type Props = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export function Tab({ href = '', className, children }: Props) {
   const pathname = usePathname();
+  const router = useRouter();
+
   const isActive = pathname === href;
 
+  const handleTabClick = () => {
+    router.push(href);
+    router.refresh(); // forcing request to the server with the current path+searchParams
+  };
+
   return (
-    <div role="tab" className="flex flex-col">
-      <Link
-        href={href}
+    <div className="flex flex-col">
+      <button
+        role="tab"
+        onClick={handleTabClick}
         className={clsx(className, 'link px-3.5 text-sm font-medium capitalize lg:text-base', {
           'text-gray-600': !isActive,
           'text-blue-800': isActive,
         })}
       >
         {children}
-      </Link>
+      </button>
       <div
         className={clsx('h-[3px] rounded-t-lg', {
           'bg-transparent': !isActive,

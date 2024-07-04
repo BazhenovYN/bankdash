24819c28c9f6045656ab3be12f99f9933a9945e5
Iurii Bazhenov <bazhenovyn@gmail.com>
2024-07-04 18:18:32 +0200

    feat: add error formatter

--- apps/api/src/trpc/router.ts
+++ apps/api/src/trpc/router.ts
@@ -5,6 +5,12 @@ import { Context } from './context';
 
 export const t = initTRPC.context<Context>().create({
   transformer: superjson,
+  errorFormatter: ({ error, shape }) => {
+    if (error.code === 'INTERNAL_SERVER_ERROR' && process.env.NODE_ENV === 'production') {
+      return { ...shape, message: 'Internal server error' };
+    }
+    return shape;
+  },
 });
 
 const isAuth = t.middleware(async ({ ctx, next }) => {
